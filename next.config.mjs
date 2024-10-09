5dca03a3867a47bf902bd85c845452bf516d57b3
Raweej <raweeroj.anon@gmail.com>
2024-10-10 03:42:59 +0700

    :sparkles: | setting auth0

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images:{
+    domains: ['lh3.googleusercontent.com','platform-lookaside.fbsbx.com','avatars.githubusercontent.com']
+  }
+};
 
 export default nextConfig;
